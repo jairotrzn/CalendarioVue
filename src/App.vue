4d97a72f9a9a3170d2cad05f11efd412aebc0d7a
Jairo <jairomora61@gmail.com>
2023-05-09 23:10:49 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <v-app>
+    <calendario/>
+  </v-app>
+</template>
+
+<script>
+
+import Calendario from './components/Calendario.vue';
+export default {
+  components: { Calendario },
+  name: 'App',
+
+  data: () => ({
+    //
+  }),
+};
+</script>
